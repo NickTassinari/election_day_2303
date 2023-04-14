0088656ff8de8038bdb106ed44e285dfc7e90e4e
Nick Tassinari <ntass7800@gmail.com>
2023-04-14 10:38:35 -0600

    finish winner method

--- lib/race.rb
+++ lib/race.rb
@@ -1,14 +1,37 @@
 class Race 
-  attr_reader :office, :candidates 
+  attr_reader :office, :candidates, :open  
 
   def initialize(office)
     @office = office 
     @candidates = []
+    @open = true 
   end
 
   def register_candidate!(candidate_info)
     candidate = Candidate.new(candidate_info)
     @candidates << candidate
     candidate 
+  end
+
+  def open? 
+    @open 
+  end
+
+  def close! 
+    @open = false 
+  end
+
+  def winner 
+    if open? 
+      false  
+    elsif  
+      @candidates[0].votes > @candidates[1].votes 
+      @candidates[0]
+    elsif  
+      @candidates[0].votes < @candidates[1].votes 
+      @candidates[1]
+    else  
+      "WE WANT A VOTE RECALL THIS THING WAS RIGGED!!"
+    end
   end
 end
--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -28,4 +28,33 @@ RSpec.describe Race do
       expect(race.candidates).to eq([candidate1, candidate2])
     end
   end
+
+  describe '#open?/#close!' do 
+    it 'can tell if election is open? and then close it' do 
+      race = Race.new("Texas Governor")
+
+      expect(race.open?).to eq(true)
+
+      race.close! 
+
+      expect(race.open?).to eq(false)
+    end
+  end
+
+  describe '@winner' do 
+    it 'can return the candidate with the most votes or a tie' do 
+    election = Election.new("2024") 
+    race = Race.new("Texas Governor")
+    candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
+    candidate2 = race.register_candidate!({name: "Roberto R", party: :republican})
+
+    election.add_candidates(candidate1)
+    election.add_candidates(candidate2)
+    candidate1.vote_for!
+    candidate1.vote_for!
+    candidate2.vote_for!
+    race.close! 
+    expect(race.winner).to eq(candidate1)
+  end
+end
 end
